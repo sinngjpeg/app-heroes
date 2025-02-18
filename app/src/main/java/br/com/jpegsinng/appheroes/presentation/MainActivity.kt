948713dda35c3bed0cfd4e4c1c45785bd55ba2eb
Ingrid Silva <ingridsilvatec@outlook.com>
2025-02-18 16:10:05 -0300

    configs navigation container

--- app/src/main/java/br/com/jpegsinng/appheroes/presentation/MainActivity.kt
+++ app/src/main/java/br/com/jpegsinng/appheroes/presentation/MainActivity.kt
@@ -2,13 +2,27 @@ package br.com.jpegsinng.appheroes.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import br.com.jpegsinng.appheroes.R
 import br.com.jpegsinng.appheroes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
+
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(
+            setOf()
+        )
     }
 }
