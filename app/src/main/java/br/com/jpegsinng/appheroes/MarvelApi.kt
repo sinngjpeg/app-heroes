6417adca34f474fb5939c0f9fcea04de383ee078
Ingrid Silva <ingridsilvatec@outlook.com>
2025-02-19 05:10:52 -0300

    maps endpoint characters

--- app/src/main/java/br/com/jpegsinng/appheroes/MarvelApi.kt
+++ app/src/main/java/br/com/jpegsinng/appheroes/MarvelApi.kt
@@ -2,11 +2,13 @@ package br.com.jpegsinng.appheroes
 
 import br.com.jpegsinng.core.data.network.response.DataWrapperResponse
 import retrofit2.http.GET
+import retrofit2.http.QueryMap
 
 interface MarvelApi {
 
-    @GET
+    @GET("characters")
     suspend fun getCharacters(
-
+        @QueryMap
+        queries: Map<String, String>
     ): DataWrapperResponse
 }
